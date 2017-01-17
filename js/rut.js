9149a61a8e8204935bf493675c8694f41ebb5f64
jesdiazs <jes.diazserrano@gmail.com>
2017-01-17 11:16:19 -0300

    se crea funcion para validar el rut cliente en javascript

--- js/rut.js
+++ js/rut.js
@@ -1,68 +1,36 @@
-function ValidaRut( rut){
-	var tmpstr = "";
-	var intlargo = getElementById("rut").value();
-	if (intlargo.length> 0)
-	{
-		crut = Objeto.value
-		largo = crut.length;
-		if ( largo <2 )
-		{
-			alert('rut inválido')
-			Objeto.focus()
-			return false;
-		}
-		for ( i=0; i <crut.length ; i++ )
-		if ( crut.charAt(i) != ' ' && crut.charAt(i) != '.' && crut.charAt(i) != '-' )
-		{
-			tmpstr = tmpstr + crut.charAt(i);
-		}
-		rut = tmpstr;
-		crut=tmpstr;
-		largo = crut.length;
- 
-		if ( largo> 2 )
-			rut = crut.substring(0, largo - 1);
-		else
-			rut = crut.charAt(0);
- 
-		dv = crut.charAt(largo-1);
- 
-		if ( rut == null || dv == null )
-		return 0;
- 
-		var dvr = '0';
-		suma = 0;
-		mul  = 2;
- 
-		for (i= rut.length-1 ; i>= 0; i--)
-		{
-			suma = suma + rut.charAt(i) * mul;
-			if (mul == 7)
-				mul = 2;
-			else
-				mul++;
-		}
- 
-		res = suma % 11;
-		if (res==1)
-			dvr = 'k';
-		else if (res==0)
-			dvr = '0';
-		else
-		{
-			dvi = 11-res;
-			dvr = dvi + "";
-		}
- 
-		if ( dvr != dv.toLowerCase() )
-		{
-			alert('El Rut Ingreso es Invalido')
-			Objeto.focus()
-			return false;
-		}
-		alert('El Rut Ingresado es Correcto!')
-		Objeto.focus()
-		return true;
-	}
+function validaRut(rut){
+ var newRut= document.getElementById('rut').value;
+ var rexp = new RegExp(/^([0-9])+\-([kK0-9])+$/);
+ if (newRut.length == 0){
+    alert('[ERROR] debe ingresar los digitos del rut');
+}else if(newRut.length>10){
+    alert("El rut no puede exceder de 10 dígitos");
+     return true;
+}else if(newRut.match(rexp)){
+     var cedula = rut.split("-");
+     var elRut = cedula[0].toArray();
+     var factor = 2;
+     var suma = 0;
+     var dv;
+     for(i=(elRut.length-1); i>=0; i--){
+         factor = factor > 7 ? 2 : factor;
+         suma += parseInt(elRut[i])*parseInt(factor++);
+     }
+     dv = 11 -(suma % 11);
+     if(dv == 11){
+         dv = 0;
+     }else if (dv == 10){
+         dv = "k";
+     }
+     if(dv == cedula[1].toLowerCase()){
+         alert("El rut es válido!!");
+         return true;
+     }else{         
+         alert("El rut es incorrecto");
+         return false;
+     }
+ }else{     
+     alert("Formato incorrecto");
+     return false;
+ }
 }
-
